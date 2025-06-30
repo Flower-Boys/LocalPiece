5b35119c798624fc27ef6c44655bcc6448b9603e
임재열 <a01092201761@gmail.com>
2025-06-30 23:01:20 +0900

    feat: 구조 완성

---
+++ BE/src/main/java/com/flowerguys/localpiece/LocalpieceApplication.java
@@ -0,0 +1,13 @@
+package com.flowerguys.localpiece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalpieceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalpieceApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/flowerguys/localpiece/LocalpieceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowerguys.localpiece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalpieceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
