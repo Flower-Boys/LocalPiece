c65b4dc16d76cc1a3713df7e34fd3b48bc69c42a
정민규 <ktfigs@gmail.com>
2025-09-11 00:16:24 +0900

    feat : 스웨거 의존성 주입 및 구현

--- BE/src/main/java/com/flowerguys/localpiece/global/config/SecurityConfig.java
+++ BE/src/main/java/com/flowerguys/localpiece/global/config/SecurityConfig.java
@@ -35,6 +35,12 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/",
+                                "/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-resources/**",
+                                "/webjars/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/blogs", "/api/blogs/**").permitAll()
                         .requestMatchers("/api/users/login", "/api/users/signup").permitAll()
                         .anyRequest().authenticated()
