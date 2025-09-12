de006c10f1f1c6b949dc130fc87f72bf2539e8a2
임재열 <a01092201761@gmail.com>
2025-09-13 04:48:47 +0900

    feat: navbar 변경, mainlaout 변경

--- FE/src/layout/MainLayout.tsx
+++ FE/src/layout/MainLayout.tsx
@@ -1,16 +1,27 @@
 import Footer from "../components/share/Footer";
-import Header from "../components/share/Header";
+import Navbar from "../components/share/Navbar";
 import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
-    <>
-      <Header />
-      <main>
-        <Outlet />
+    <div className="flex min-h-screen bg-gray-50">
+      {/* 왼쪽 네브바 (데스크탑 전용) */}
+      <aside className="hidden md:block w-60 border-r border-gray-200 p-4">
+        <Navbar />
+      </aside>
+
+      {/* 메인 콘텐츠 */}
+      <main className="flex-1 flex justify-center">
+        <div className="w-full max-w-2xl p-4">
+          <Outlet />
+        </div>
       </main>
-      <Footer />
-    </>
+
+      {/* 푸터 (모바일에서만 고정) */}
+      <div className="fixed bottom-0 left-0 right-0 md:hidden border-t border-gray-200 bg-white">
+        <Footer />
+      </div>
+    </div>
   );
 };
 
