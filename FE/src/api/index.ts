f66843e2a6d23856189166a0301fcecc272b413e
임재열 <a01092201761@gmail.com>
2025-09-14 11:08:33 +0900

    feat: basURL 변경

--- FE/src/api/index.ts
+++ FE/src/api/index.ts
@@ -1,15 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
 // API 클라이언트 인스턴스 생성
 const apiClient = axios.create({
   // 벡엔드 서버의 기본 URL을 설정합니다.
   // 이제 모든 요청은 이 주소를 기준으로 이루어집니다.
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "https://localpiece.duckdns.org/api",
   // 요청 타임아웃을 10초로 설정합니다.
   timeout: 10000,
   // 요청 헤더에 JSON 형식임을 명시합니다.
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
