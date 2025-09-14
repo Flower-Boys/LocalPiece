d4d328027bae6294c98e57890accdaf26e2e0785
임재열 <a01092201761@gmail.com>
2025-09-14 14:08:21 +0900

    feat: placeholder 변경

--- FE/src/components/home/SearchBar.tsx
+++ FE/src/components/home/SearchBar.tsx
@@ -4,7 +4,7 @@ const SearchBar = () => {
   return (
     <div className="bg-white rounded-full shadow-lg max-w-4xl h-20 mx-auto flex items-center overflow-hidden px-4 border border-gray-300">
       {/* 위치 입력 */}
-      <input type="text" placeholder="여행지 검색" className="flex-1 px-6 py-3 text-lg text-gray-900 focus:outline-none" />
+      <input type="text" placeholder="경상북도 여행지를 검색해보세요!" className="flex-1 px-6 py-3 text-lg text-gray-900 focus:outline-none" />
 
       {/* 검색 버튼 */}
       <button className="bg-red-500 hover:bg-red-600 text-white rounded-full w-12 h-12 flex items-center justify-center ml-2">
