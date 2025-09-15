ee493f44606cb7f457a9fa4129fd8bee8559b17e
임재열 <a01092201761@gmail.com>
2025-09-15 17:57:39 +0900

    feat: toast 추가, token 관련 로직 추가

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -1,11 +1,29 @@
-import "./index.css"; // ✅ Tailwind import
-import React from "react";
+import "./index.css";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import { useAuthStore } from "./store/authStore";
+import { Toaster } from "react-hot-toast";
+
+const InitAuth = ({ children }: { children: React.ReactNode }) => {
+  const login = useAuthStore((state) => state.login);
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      login(token);
+    }
+  }, [login]);
+
+  return <>{children}</>;
+};
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <InitAuth>
+      <RouterProvider router={router} />
+      <Toaster position="top-center" reverseOrder={false} />
+    </InitAuth>
   </React.StrictMode>
 );
