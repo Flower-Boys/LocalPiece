7ba8bbfc968bb7fc148ad826d66764aca33e36d3
임재열 <a01092201761@gmail.com>
2025-09-16 19:18:34 +0900

    feat: Home.tsx에 보낼 핸들러 추가 및 여행지, 카테고리 컴포넌트 추가

--- FE/src/components/home/SearchBar.tsx
+++ FE/src/components/home/SearchBar.tsx
@@ -1,13 +1,43 @@
+import { useState } from "react";
 import { Search } from "lucide-react";
+import RegionSelect from "./RegionSelect";
+import ContentTypeSelect from "./ContentTypeSelect";
 
-const SearchBar = () => {
+
+type Props = {
+  onSearch: (params: { sigunguCode: string; contentTypeId: string }) => void;
+};
+
+const SearchBar = ({ onSearch }: Props) => {
+  const [selectedRegion, setSelectedRegion] = useState("");
+  const [selectedType, setSelectedType] = useState("");
+
+  const handleSearch = () => {
+    onSearch({
+      sigunguCode: selectedRegion,
+      contentTypeId: selectedType,
+    });
+  };
+
   return (
-    <div className="bg-white rounded-full shadow-lg max-w-4xl h-20 mx-auto flex items-center overflow-hidden px-4 border border-gray-300">
-      {/* 위치 입력 */}
-      <input type="text" placeholder="경상북도 여행지를 검색해보세요!" className="flex-1 px-6 py-3 text-lg text-gray-900 focus:outline-none" />
+    <div className="bg-white rounded-full shadow-md max-w-4xl h-16 mx-auto flex items-center border border-gray-300">
+      {/* 여행지 */}
+      <div className="flex-1 px-6 py-2 cursor-pointer hover:bg-gray-100 rounded-full transition">
+        <RegionSelect value={selectedRegion} onChange={setSelectedRegion} />
+      </div>
+
+      <div className="w-px h-8 bg-gray-300" />
+
+      {/* 카테고리 */}
+      <div className="flex-1 px-6 py-2 cursor-pointer hover:bg-gray-100 rounded-full transition">
+        <ContentTypeSelect value={selectedType} onChange={setSelectedType} />
+      </div>
 
       {/* 검색 버튼 */}
-      <button className="bg-red-500 hover:bg-red-600 text-white rounded-full w-12 h-12 flex items-center justify-center ml-2">
+      <button
+        onClick={handleSearch}
+        className="bg-rose-500 hover:bg-rose-600 text-white rounded-full w-12 h-12 flex items-center justify-center mx-4"
+      >
         <Search size={20} />
       </button>
     </div>
