4d9031f72dd93eaca3afd7606d9bb1166793f7a9
임재열 <a01092201761@gmail.com>
2025-10-21 15:16:58 +0900

    feat: 블로그 수정 페이지 router 연결

--- FE/src/router/index.tsx
+++ FE/src/router/index.tsx
@@ -6,6 +6,7 @@ import Blog from "../pages/blogs/BlogPage";
 import NotFound from "../pages/errors/NotFound";
 import BlogWrite from "../pages/blogs/BlogWrite";
 import BlogDetail from "@/pages/blogs/BlogDetail";
+import BlogEditPage from "@/pages/blogs/BlogEditPage";
 import TourDetail from "@/components/tour/TourDetail";
 import { AiTravelLanding, CategorySelect, AiResultPreview } from "@/pages/aitravel";
 import MyPage from "@/components/mypage/MyPage";
@@ -30,6 +31,7 @@ const router = createBrowserRouter([
       { path: "/ai/travel/builder", element: <CategorySelect /> },
       { path: "/ai/travel/result", element: <AiResultPreview /> },
       { path: "/mypage", element: <MyPage /> },
+      { path: "/blog/:id/edit", element: <BlogEditPage /> },
     ],
   },
   // { path: "/login", element: <Login /> },
