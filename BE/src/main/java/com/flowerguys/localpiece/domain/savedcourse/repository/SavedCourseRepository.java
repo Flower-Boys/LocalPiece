19cf20d218b29b62ffdc748d2dd77162a327f7d9
정민규 <ktfigs@gmail.com>
2025-10-24 17:04:01 +0900

    fix : 여행 코스 N+1 오류 수정

--- BE/src/main/java/com/flowerguys/localpiece/domain/savedcourse/repository/SavedCourseRepository.java
+++ BE/src/main/java/com/flowerguys/localpiece/domain/savedcourse/repository/SavedCourseRepository.java
@@ -11,7 +11,6 @@ public interface SavedCourseRepository extends JpaRepository<SavedCourse, Long> 
     List<SavedCourse> findAllByUserEmailOrderByCreatedAtDesc(String email);
     @Query("SELECT DISTINCT sc FROM SavedCourse sc " +
            "LEFT JOIN FETCH sc.days d " +
-           "LEFT JOIN FETCH d.route " +
            "WHERE sc.id = :courseId AND sc.user.email = :email")
     Optional<SavedCourse> findDetailsByIdAndUserEmail(@Param("courseId") Long courseId, @Param("email") String email);
     Optional<SavedCourse> findByIdAndUserEmail(Long id, String email);
