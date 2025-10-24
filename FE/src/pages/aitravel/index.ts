dc791bbe4845cbb126523f31b3a5c4623b09c8b6
JYeol97 <a01092201761@gmail.com>
2025-10-24 18:11:06 +0900

    feat: 디테일페이지 index 규칙에 추가

--- FE/src/pages/aitravel/index.ts
+++ FE/src/pages/aitravel/index.ts
@@ -1,3 +1,4 @@
 export { default as AiTravelLanding } from "./AiTravelLanding";
 export { default as CategorySelect } from "./CategorySelect";
 export { default as AiResultPreview } from "./AiResultPreview";
+export { default as AiTravelDetail } from "./AiTravelDetail"
