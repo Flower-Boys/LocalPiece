ed55a677593c7e977912784ac2f4b329f7f928dd
임재열 <a01092201761@gmail.com>
2025-10-25 16:06:25 +0900

    feat: 여행지 추천 관련 route 규칙 추가

--- FE/src/pages/aitravel/index.ts
+++ FE/src/pages/aitravel/index.ts
@@ -1,4 +1,5 @@
 export { default as AiTravelLanding } from "./AiTravelLanding";
 export { default as CategorySelect } from "./CategorySelect";
 export { default as AiResultPreview } from "./AiResultPreview";
-export { default as AiTravelDetail } from "./AiTravelDetail"
+export { default as AiTravelDetailPretty } from "../../components/aiTravel/AiTravelDetailPretty";
+export { default as AiTravelDetail } from "./AiTravelDetailPage";
